2f1cb68f605afaccd5156666508aa7f348ab1315
Dimo Grigorov <drgrigorov@gmail.com>
2017-06-28 21:35:26 +0300

    Added simple scanner application. Now in working state.

--- test/scanner.cpp
+++ test/scanner.cpp
@@ -8,25 +8,75 @@
 #endif
 // // Hardware configuration // // Set up nRF24L01 radio on
 //SPI bus plus pins 8 & 9
-void setup(void);
-void loop(void);
+void setup(RF24& radio);
+void loop(RF24& radio);
 
 
-RF24 radio(SUNXI_GPI(14), SUNXI_GPI(15), "/dev/spidev2.0"); // // Channel info 
+//RF24 radio(SUNXI_GPI(14), SUNXI_GPI(15), "/dev/spidev2.0"); // // Channel info 
 //
 const short num_channels = 128;
 short values[num_channels]; //
 
-int main(int argv, char** argc)
+void PrintUsage( const char* szName )
 {
-  setup();
-  while (1) loop();
+	printf("Usage: %s <-d device_path>\n", szName );
+}
+
+int main(int argc, char** argv)
+{
+	char* szDeviceName = 0;
+
+	if (argc < 2)
+	{
+		printf("Not enough arguments\n");
+		PrintUsage( argv[0] );
+		return 1;
+	}
+
+	char opt = 0;
+	while(opt != -1)
+	{
+		opt = getopt( argc, argv, "d:" );
+		//wtf 255?!
+		if ( -1 == opt || 255 == opt ) break;
+		switch(opt)
+		{
+			case 'd':
+				szDeviceName = optarg;
+				break;
+			case '?':
+			default:
+				printf("Unknown adgument %c[%d]\n", opt,opt );
+				PrintUsage( argv[0] );
+				return 1;
+				break;
+		}
+	}
+
+	int CE = 0, CSN = 0;
+
+	//lame but works for now
+	if ( strstr( szDeviceName, "spidev2" ) )
+	{
+		CE = UEXT1_CE;
+		CSN = UEXT1_CSN;
+	}
+	else
+	{
+		CE = UEXT2_CE;
+		CSN = UEXT2_CSN;
+	}
+
+	RF24 radio(CE, CSN, szDeviceName);
+
+  setup( radio );
+  while (1) loop( radio );
 
   return 0;
 }
 
 // Setup //
-void setup(void) {
+void setup(RF24& radio) {
   //
   // Print preamble
   //
@@ -37,10 +87,13 @@ void setup(void) {
   //
   radio.begin();
   radio.setAutoAck(false);
+  radio.disableCRC();
+  radio.setRetries( 0, 0 );
   // Get into standby mode
   radio.startListening();
   radio.stopListening();
   // Print out header, high then low digit
+  radio.printDetails();
   int i = 0;
   while ( i < num_channels )
   {
@@ -59,7 +112,7 @@ void setup(void) {
 // // Loop //
 const short num_reps = 100;
 
-void loop(void) {
+void loop(RF24& radio) {
   // Clear measurement values
   memset(values,0,num_channels);
   // Scan all channels num_reps times
